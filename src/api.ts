9e4fa099606111bf5e92910f0069f6d328ddd623
LeeYongwoo-kor <newlyo1205@naver.com>
2022-02-06 22:35:30 +0900

    add api/utils

---
+++ src/api.ts
@@ -0,0 +1,27 @@
+const API_KEY = "b4b6dae2fb016b7e49c54bdb2f7aae9d";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maxinum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (res) => res.json()
+  );
+}
